a6e207d54eef694a775c972946a795ff1b45972a
en44no <nahuel.manya99@hotmail.com>
2021-06-16 17:08:14 -0300

    added a down arrow

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -4,12 +4,19 @@ import ReactSpinner from 'react-bootstrap-spinner';
 import '../css/Cover.css';
 
 export const Cover = () => {
+  const goToTop = () => {
+    window.scrollTo({ top: 1000, left: 0, behavior: 'smooth' });
+  };
   return (
     <div className="cover-container">
       <video className="video" src={coverVideo} autoPlay loop muted></video>
       <h1>Hello, I'm Nahuel</h1>
       <h4 className="mb-4">A programmer in process</h4>
       <ReactSpinner type="border" color="light" size="1.5" />
+      <i
+        className="down-arrow fad fa-chevron-double-down"
+        onClick={goToTop}
+      ></i>
     </div>
   );
 };
