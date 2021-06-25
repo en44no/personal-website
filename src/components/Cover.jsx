febc64616f2318d5d9210e1d94f8ae5227cb512a
en44no <nahuel.manya99@hotmail.com>
2021-06-25 16:17:22 -0300

    change react-bootstrap-spinner to fontawesome spinner

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import coverVideo from '.././media/coverVideo.mov';
-import ReactSpinner from 'react-bootstrap-spinner';
 import '../css/Cover.css';
 
 export const Cover = () => {
-  const goToAbout = () => {
-    window.scrollTo({ top: 720, left: 0, behavior: 'smooth' });
-  };
   return (
     <div className="cover-container">
       <video className="video" src={coverVideo} autoPlay loop muted></video>
       <h1>Hello, I'm Nahuel</h1>
       <h4 className="mb-4">A programmer in process</h4>
-      <div className="spinner">
-        <ReactSpinner type="border" color="light" size="1.5" />
-      </div>
-      <i
-        className="down-arrow fad fa-chevron-double-down"
-        onClick={goToAbout}
-      ></i>
+      <i class="spinner fas fa-spinner fa-spin"></i>
+      <i className="down-arrow fad fa-chevron-double-down"></i>
     </div>
   );
 };
