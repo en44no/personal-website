b73282d94a60c9634490f905d37e36b19b39cab6
en44no <nahuel.manya99@hotmail.com>
2021-06-29 23:40:51 -0300

    changed local video to hosted gif

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import coverVideo from '.././media/coverVideo.mov';
 import '../css/Cover.css';
 
 export const Cover = () => {
   return (
     <div className="cover-container">
-      <video className="video" src={coverVideo} autoPlay loop muted></video>
+      <img
+        className="video"
+        src="https://media.giphy.com/media/9OEf08mC8sUV7SA2nS/giphy.gif"
+        alt="why"
+      ></img>
       <h1>Hello, I'm Nahuel</h1>
       <h4 className="mb-4">A programmer in process</h4>
       <i class="spinner fas fa-spinner fa-spin"></i>
