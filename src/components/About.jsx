78535769ad3e45f6570e990794479621b56002ee
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-09-24 19:38:38 -0300

    Added my CV

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../css/About.css';
 import mePhoto from '.././media/me.jpeg';
-import CV from '.././media/test.pdf';
+import CV from '.././media/nahuelMarquezCV.pdf';
 
 export const About = () => {
   return (
